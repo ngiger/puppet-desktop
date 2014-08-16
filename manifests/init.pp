eb86df99cd4ae0d7946dd9378d4c2a5e906a6b45
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-08-17 00:04:01 +0200

    Removed cruft

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,86 +36,19 @@
 # Copyright 2014 Your name here, unless otherwise noted.
 #
 class desktop {
-  # import "hostnames/*.pp"
-  # replace this by hiera-variables
-  notify{"hostname $hostname and $fqdn looking for classes_for_${hostname}": }
-  include apt
-  Exec['apt_update'] -> Package <| |>
 
-  $hostname_classes = hiera("classes_for_${hostname}", '')
-  
-  if ("classes_for_$hostname" != "") {
-    file{"/etc/hostname_classes":
-      content => "$hostname_classes\n",
-#      require => [ Class[$hostname_classes], ] # fails when more than 1 class given, eg x2go::common
+  $display_manager =  hiera('X::display_manager', false)
+  if ($display_manager) { package{$display_manager: }
+    service{$display_manager:
+      ensure  => running,
+      require => Package[$display_manager],
     }
-    include $hostname_classes
   }
 
-  # https://forge.puppetlabs.com/puppetlabs/stdlib
-  # file_line: This resource ensures that a given line is contained within a file. You can also use "match" to replace existing lines.
-  # loadyaml Load a YAML file containing an array, string, or hash, and return the data in the corresponding native data type.
-  #  str2saltedsha512 This converts a string to a salted-SHA512 password hash (which is used for OS X versions >= 10.7). Given any simple string, you will get a hex version of a salted-SHA512 password hash that can be inserted into your Puppet manifests as a valid password attribute.
- 
-  # unix chpasswd [options] DESCRIPTION     The chpasswd command reads a list of user name and password pairs from standard input and uses this information to update a group of existing users.
-  # Each line is of the format:
-  # user_name:password
-
-  $hostname_packages = hiera("packages_for_${hostname}", '')
-  
-  if ("packages_for_$hostname" != "") {
-    file{"/etc/packages_for_$hostname":
-      content => join($hostname_packages, "\n"),
-    }
-    ensure_packages[$hostname_packages]
-  }
-# ensure_resource('user', 'dan', {'ensure' => 'present' }) # for users to be listed on the login site
-
-  include apt
-  if ( "$operatingsystem" == "Debian") {
-    apt::source { 'debian_wheezy':
-      location          => hiera('apt::source:location', 'http://mirror.switch.ch/ftp/mirror/debian/'),
-      release           => hiera('apt::source:release', 'wheezy'),
-      repos             => hiera('apt::source:repos', 'main contrib non-free'),
-    #  required_packages => hiera('apt::source:required_packages', 'debian-keyring debian-archive-keyring'),
-    #  key               => hiera('apt::source:key', '55BE302B'),
-    #  key_server        => hiera('apt::source:key_server', 'subkeys.pgp.net'),
-    #  pin               => hiera('apt::source:pin', '-10'),
-      include_src       => hiera('apt::source:include_src', true),
-    }
-
-    apt::source { 'debian_security':
-      location          => hiera('apt::source:security:location', 'http://security.debian.org/'),
-      release           => hiera('apt::source:security:release', 'wheezy/updates'),
-      repos             => hiera('apt::source:security:repos', 'main contrib non-free'),
-      include_src       => hiera('apt::source:security:include_src', true),
-    }
-
-    
-  } else {
-    file{'/etc/apt/sources.list.d/debian_wheezy.list': ensure => absent }
-    file{'/etc/apt/sources.list.d/debian_security.list': ensure => absent } 
-  }
+  $window_manager =  hiera('X::window_manager', false)
+  if ($window_manager) { package{$window_manager:} }
 
   if ( "$operatingsystem" == "Ubuntu") {
-    # :deb http://ppa.launchpad.net/x2go/stable/ubuntu trusty main
-  #     apt::ppa { "ppa:x2go/stable": }
-    apt::source { 'trusty-CH':
-      location   => 'http://ch.archive.ubuntu.com/ubuntu',
-      release    => 'trusty',
-      repos      => 'main restricted universe multiverse',
-    }
-
-    apt::source { 'trusty-security':
-      location   => 'http://security.ubuntu.com/ubuntu',
-      release    => 'trusty-security',
-      repos      => 'main restricted universe multiverse',
-    }
-    apt::ppa { "universe": }
-    apt::ppa { "multiverse": }
-    Apt::Source['trusty-CH',  'trusty-security'] ->  Exec['apt_update'] 
-    Apt::Ppa['universe',  'multiverse'] ->  Exec['apt_update']
-    
     ensure_packages[
       # 'gnome-control-center', 'gnome-session',
       'libreoffice-gtk3', 'libreoffice', 'libreoffice-l10n-de', 'libreoffice-l10n-fr',
@@ -132,7 +65,7 @@ class desktop {
     ]
   }
   ensure_packages['kdm','libreoffice-kde', 
+    'nemo',
     'openjdk-7-jdk',
   ]
-#    ensure_packages['nemo'] # cannot be installed because auf libgail-3-0
 }
